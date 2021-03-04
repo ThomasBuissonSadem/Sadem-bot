b4bc6af405beecd2ac4a932217c70188abef6dee
Thomas Buisson <thomas.buisson@sadem-group.fr>
2021-03-04 15:59:17 +0100

    testing working deployment

--- bot/main.py
+++ bot/main.py
@@ -9,15 +9,6 @@ token = os.getenv("DISCORD_BOT_TOKEN")
 async def on_ready():
     print('Démarrage du Bot : {0.user}'.format(client))
 
-@client.event
-async def on_message(message):
-    if message.content.startswith('hello'):
-      for member in message.guild.members:
-        print(f'Membre: {member} \n')
-        if 'Administrateurs' in member.roles:
-          print(f'MESSAGE')
-          await member.send("Hello")
-
 # Commande permettant de supprimer 5 messages sur le channel concerné
 @client.command()
 async def clear(ctx, amount=5) :
