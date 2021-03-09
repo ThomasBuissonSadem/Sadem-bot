0028324e9dfaf490a2a3335b121fd82990205ee6
Thomas Buisson <thomas.buisson@sadem-group.fr>
2021-03-09 10:20:37 +0100

    Using object to retrieve guild

--- bot/main.py
+++ bot/main.py
@@ -1,30 +1,27 @@
 import discord
+import asyncio
 from discord.ext import commands
 import os
 
 # Variable Globales
 intents = discord.Intents.all()
 token = os.getenv("DISCORD_BOT_TOKEN")
-guild_id = os.getenv("GUILD_ID")
 client = commands.Bot(command_prefix=".", intents=intents)
 
 # Traitement au démarrage du bot
 @client.event
 async def on_ready():
     print('Démarrage du Bot : {0.user}'.format(client))
-    print(f'ID : {client.guild.id}')
 
 # Envoi un message aux admin lorsqu'un utilisateur rejoint le serveur
 @client.event
 async def on_member_join(newMember):
-    try:
-        guild = client.get_guild(int(guild_id))
-        for member in guild.members:
-            for role in member.roles:
-                if(role.name.lower() == "Administrateurs".lower()):
-                    await member.send(f'L\'utilisateur @{newMember.name} vient de rejoindre le serveur {guild.name}.')
-    except discord.ext.commands.errors.MissingRole:
-        await ctx.send("vous n'avez pas la permission d'utiliser cette commande.")
+    guild = newMember.guild
+    for member in guild.members:
+        for role in member.roles:
+            if(role.name.lower() == "Administrateurs".lower()):
+                await member.send(f'L\'utilisateur {newMember.name} vient de rejoindre le serveur {guild.name}.')
+
 
 # Commande permettant de supprimer 10 messages sur le channel concerné
 @client.command()
@@ -35,8 +32,7 @@ async def clear(ctx, amount=10):
 @client.command()
 @commands.has_role('Administrateurs')
 async def testing(ctx):
-    await ctx.send(f'{guild_id}')
-    await ctx.send(f'{client.guild.id}')
-        
+    await ctx.send('testing')
+
 
 client.run(token)
